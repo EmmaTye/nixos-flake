bf440afb1adc979e991ac37f3821c95c80f7d15b
EmmaTye <tyelouiseemma@gmail.com>
2025-10-09 15:12:41 +0000

    init

---
+++ flake.nix
@@ -0,0 +1,50 @@
+{
+  description = "NixOS flake";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.05";
+
+    # Windows Subsystem for Linux
+    nixos-wsl.url = "github:nix-community/NixOS-WSL/main";
+
+    home-manager = {
+      url = "github:nix-community/home-manager/release-25.05";
+      # The `follows` keyword in inputs is used for inheritance.
+      # Here, `inputs.nixpkgs` of home-manager is kept consistent with
+      # the `inputs.nixpkgs` of the current flake,
+      # to avoid problems caused by different versions of nixpkgs.
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+
+    nixvim = {
+      url = "github:nix-community/nixvim/nixos-25.05";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs = inputs@{ self, nixpkgs, nixos-wsl, home-manager, nixvim, ... }: {
+    nixosConfigurations.nixos = nixpkgs.lib.nixosSystem {
+      system = "x86_64-linux";
+
+      modules = [
+	# Windows Subsystem for Linux as a NixOS module
+        nixos-wsl.nixosModules.default {
+          system.stateVersion = "25.05";
+          wsl.enable = true;
+        }
+
+	# home-manager as a NixOS module	
+        home-manager.nixosModules.home-manager {
+          home-manager.useGlobalPkgs = true;
+          home-manager.useUserPackages = true;
+          home-manager.users.nixos = import ./home.nix;
+          home-manager.extraSpecialArgs = { inherit inputs; };
+        }
+
+        # System-wide configuration
+        ./system.nix
+      ];
+    };
+  };
+}
+
